0c69c147af05b669c94e4315ae2567be04045b5a
bahgaat <bahgat.reda90@gmail.com>
2020-09-12 15:19:26 +0200

    phase 1 third part

--- proj2/byog/Core/Position.java
+++ proj2/byog/Core/Position.java
@@ -1,13 +1,26 @@
 package byog.Core;
 
-class Position {
+import byog.TileEngine.TERenderer;
+import byog.TileEngine.TETile;
+import edu.princeton.cs.introcs.StdDraw;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.io.Serializable;
+
+import static com.sun.java.accessibility.util.AWTEventMonitor.addMouseMotionListener;
+
+
+class Position implements Serializable {
     int _x;
     int _y;
+    TETile _type;
 
-    Position(int x, int y) {
+    Position(int x, int y, TETile type) {
         _x = x;
         _y = y;
+        _type = type;
     }
-
 
 }
